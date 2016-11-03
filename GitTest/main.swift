9e362c62e87567b9e5b6ac7e7542b12d71cec80e
ttaanngg <tangweiqiang@hotmail.com>
2016-11-03 13:02:47 +0800

    Initial Commit

---
+++ GitTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitTest
+//
+//  Created by Weiqiang TANG on 2016/11/3.
+//  Copyright © 2016年 fusion. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
